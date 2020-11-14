b3a628b8f488876609f9720ee61b147fce746bbe
Mrqinforce <1293340422@qq.com>
2020-11-14 17:15:16 +0800

    使用Swagger2

--- soft-news-service-api/src/main/java/com/soft1851/api/controller/user/UserControllerApi.java
+++ soft-news-service-api/src/main/java/com/soft1851/api/controller/user/UserControllerApi.java
@@ -1,6 +1,11 @@
 package com.soft1851.api.controller.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.soft1851.result.GraceResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @ClassName $(Name)
@@ -9,12 +14,24 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @Date 2020/11/14
  * @Version 1.0
  **/
+@Api(value = "用户信息相关Controller", tags = {"用户信息相关Controller"})
+@RequestMapping("user")
 public interface UserControllerApi {
     /**
-     * 获取所有用户
+     * 获取所有用户信息
      *
-     * @return users: 所有用户信息
+     * @return 获取所有用户信息
      */
-    @GetMapping("/users")
-    Object getUsers();
+   @ApiOperation(value = "获取所有用户信息", notes = "获取所有用户信息", httpMethod = "POST")
+   @PostMapping("/all")
+   GraceResult getAllUsers();
+
+   /**
+    * 获取用户基本信息
+    * @param userId:用户id
+    * @return 用户基本信息
+    */
+   @ApiOperation(value = "获取用户基本信息", notes = "获取用户基本信息", httpMethod = "POST")
+   @PostMapping("/userInfo")
+   GraceResult getUserInfo(@RequestParam String userId);
 }
--- soft-news-service-user/src/main/java/com/soft1851/user/controller/UserController.java
+++ soft-news-service-user/src/main/java/com/soft1851/user/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.soft1851.user.controller;
 
 import com.soft1851.api.controller.user.UserControllerApi;
+import com.soft1851.pojo.AppUser;
 import com.soft1851.result.GraceResult;
 import com.soft1851.user.mapper.AppUserMapper;
 import org.springframework.web.bind.annotation.RestController;
+import tk.mybatis.mapper.entity.Example;
 
 import javax.annotation.Resource;
 
@@ -18,8 +20,18 @@ import javax.annotation.Resource;
 public class UserController implements UserControllerApi {
     @Resource
     private AppUserMapper appUserMapper;
+
     @Override
-    public Object getUsers() {
+    public GraceResult getAllUsers() {
         return GraceResult.ok(appUserMapper.selectAll());
+    }
+
+    @Override
+    public GraceResult getUserInfo(String userId) {
+        Example userExample = new Example(AppUser.class);
+        Example.Criteria userCriteria = userExample.createCriteria();
+        userCriteria.andEqualTo("id", userId);
+        AppUser user = appUserMapper.selectOneByExample(userExample);
+        return GraceResult.ok(user);
     }
 }
