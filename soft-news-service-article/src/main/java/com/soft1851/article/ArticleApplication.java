cf3522baffa6eec13f02036e8f7f48d64fdb2fdc
Mrqinforce <1293340422@qq.com>
2020-11-24 19:54:42 +0800

    文章服务模块准备

--- soft-news-service-article/src/main/java/com/soft1851/article/ArticleApplication.java
+++ soft-news-service-article/src/main/java/com/soft1851/article/ArticleApplication.java
@@ -13,6 +13,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ArticleApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ArticleApplication.class, args);
+        SpringApplication.run(ArticleApplication.class, args)
     }
 }
